2c3913a674a9afce26484535f5047e0532c6999b
jonsamp <sampjon@gmail.com>
2020-05-22 21:07:13 -0400

    stack navigator setup

--- App.tsx
+++ App.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+function HomeScreen(props: any) {
+  const { navigation } = props;
+
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Entry"
+        onPress={() => navigation.navigate("Entry")}
+      />
+    </View>
+  );
+}
+
+function EntryScreen(props: any) {
+  const { navigation } = props;
+  return (
+    <View style={styles.container}>
+      <Text>Entry Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={HomeScreen} name="Home" />
+        <Stack.Screen component={EntryScreen} name="Entry" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
